8ce4945cd0c590e088145ec24e8f7bf2a7ce0293
andrej-naumov <andrejs@list.ru>
2024-06-13 15:47:28 +0200

    Lesson 08 - homework - added CustomerService and Controller

---
+++ src/src/main/java/com/example/g33_shop/service/interfaces/ProductService.java
@@ -0,0 +1,21 @@
+package com.example.g33_shop.service.interfaces;
+
+
+import com.example.g33_shop.domain.entity.Product;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface ProductService {
+
+    Product save(Product product);
+    List<Product> getAllActiveProducts();
+    Product getById(Long id);
+    Product update(Product product);
+    void deleteById(Long id);
+    void deleteByTitle(String title);
+    void restoreById(Long id);
+    long getAllActiveProductsQuantity();
+    BigDecimal getAllActiveProductsTotalPrice();
+    BigDecimal getAllActiveProductsAveragePrice();
+}
