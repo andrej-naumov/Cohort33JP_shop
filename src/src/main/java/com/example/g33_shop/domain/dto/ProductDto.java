23f5bf0b1c2e90692a51a22a17c279d3c580f46a
andrej-naumov <andrejs@list.ru>
2024-06-18 21:10:22 +0200

    Lesson 10 - added ProductDto.java

---
+++ src/src/main/java/com/example/g33_shop/domain/dto/ProductDto.java
@@ -0,0 +1,67 @@
+package com.example.g33_shop.domain.dto;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Schema(description = "Class that describes Product")
+public class ProductDto {
+
+    @Schema(
+            description = "Product unique identifier",
+            example = "777",
+            accessMode = Schema.AccessMode.READ_ONLY
+    )
+    private Long id;
+
+    @Schema(description = "Product title", example = "Banana")
+    private String title;
+
+    @Schema(description = "Product price", example = "190.00")
+    private BigDecimal price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductDto that = (ProductDto) o;
+        return Objects.equals(id, that.id) && Objects.equals(title, that.title) && Objects.equals(price, that.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, price);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Product DTO: id - %d, title - %s, price - %s",
+                id, title, price);
+    }
+}
